8365551725a893491107825dd5b0bbf243faeb25
Ivan Yu. Bondarenko <bond005@yandex.ru>
2019-03-28 22:15:30 +0700

    Calculation of quality has been speed up.

--- deep_ner/quality.py
+++ deep_ner/quality.py
@@ -91,6 +91,8 @@ def find_pairs_of_named_entities(true_entities: List[int], predicted_entities: L
         best_pairs = list(filter(lambda it1: it1 in similarity_dict, map(lambda it2: (it2, it2), range(n_true))))
         best_similarity_sum = sum(map(lambda it: similarity_dict[it][0], best_pairs))
     else:
+        N_MAX_COMB = 10
+        counter = 1
         if n_true < n_predicted:
             for c in comb(n_predicted, n_true):
                 pairs = list(filter(
@@ -105,6 +107,33 @@ def find_pairs_of_named_entities(true_entities: List[int], predicted_entities: L
                     best_similarity_sum = similarity_sum
                     best_pairs = copy.deepcopy(pairs)
                 del pairs
+                counter += 1
+                if counter > N_MAX_COMB:
+                    break
+            pairs = []
+            used_indices = set()
+            for true_idx in range(n_true):
+                best_pred_idx = None
+                best_similarity = -1.0
+                for pred_idx in filter(lambda it: it not in used_indices, range(n_predicted)):
+                    pair_candidate = (true_idx, pred_idx)
+                    if pair_candidate in similarity_dict:
+                        if similarity_dict[pair_candidate][0] > best_similarity:
+                            best_similarity = similarity_dict[pair_candidate][0]
+                            best_pred_idx = pred_idx
+                if best_pred_idx is None:
+                    break
+                used_indices.add(best_pred_idx)
+                pairs.append((true_idx, best_pred_idx))
+            if len(pairs) > 0:
+                similarity_sum = sum(map(lambda it: similarity_dict[it][0], pairs))
+            else:
+                similarity_sum = 0.0
+            if similarity_sum > best_similarity_sum:
+                best_similarity_sum = similarity_sum
+                best_pairs = copy.deepcopy(pairs)
+            del pairs
+            del used_indices
         else:
             for c in comb(n_true, n_predicted):
                 pairs = list(filter(
@@ -119,6 +148,33 @@ def find_pairs_of_named_entities(true_entities: List[int], predicted_entities: L
                     best_similarity_sum = similarity_sum
                     best_pairs = copy.deepcopy(pairs)
                 del pairs
+                counter += 1
+                if counter > N_MAX_COMB:
+                    break
+            pairs = []
+            used_indices = set()
+            for pred_idx in range(n_predicted):
+                best_true_idx = None
+                best_similarity = -1.0
+                for true_idx in filter(lambda it: it not in used_indices, range(n_true)):
+                    pair_candidate = (true_idx, pred_idx)
+                    if pair_candidate in similarity_dict:
+                        if similarity_dict[pair_candidate][0] > best_similarity:
+                            best_similarity = similarity_dict[pair_candidate][0]
+                            best_true_idx = true_idx
+                if best_true_idx is None:
+                    break
+                used_indices.add(best_true_idx)
+                pairs.append((best_true_idx, pred_idx))
+            if len(pairs) > 0:
+                similarity_sum = sum(map(lambda it: similarity_dict[it][0], pairs))
+            else:
+                similarity_sum = 0.0
+            if similarity_sum > best_similarity_sum:
+                best_similarity_sum = similarity_sum
+                best_pairs = copy.deepcopy(pairs)
+            del pairs
+            del used_indices
     return best_similarity_sum, best_pairs
 
 
